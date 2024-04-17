7c2f8f67097b5bb3fa6bcacc883e517063807bd3
korbin0704 <korbinstandstrong@gmail.com>
2024-04-17 21:52:37 +0800

    Code cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useEffect } from 'react';
 import './App.scss';
 import PageRoutes from './component/PageRoutes';
-import { useLocation, useNavigate, useNavigation } from 'react-router-dom';
 
 function App() {
   useEffect(() => {
-    // const location = useLocation()
-    // location.href = "/juicyadventure"
   }, [])
 
   return (
